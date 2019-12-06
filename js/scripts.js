be7bde0a5260131377ad1a42ef1d4c71d4d02d5a
Anna <annayoakum@ufl.edu>
2019-12-06 00:41:56 -0600

    Add content to contact page and add accordion functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,24 @@
-// Selection of HTML objects
+//Hamburger
+
+// Hamburger: Selection of HTML objects
 
 const burger = document.querySelector('.burger i');
 const nav = document.querySelector('.nav');
 
-// Defining a function
+// Hamburger: Defining a function
 function toggleNav() {
 burger.classList.toggle('fa-bars');
 burger.classList.toggle('fa-times');
 nav.classList.toggle('nav-active');
 }
 
-// Calling the function after click event occurs
+// Hamburger: Calling the function after click event occurs
 burger.addEventListener('click', function() {
 toggleNav();
 });
 
-// Read more, read less expandable
+
+// Expandable: Read more, read less
 
 function myExpandable() {
   var dots = document.getElementById("dots");
@@ -32,3 +35,34 @@ function myExpandable() {
     moreText.style.display = "inline";
   }
 }
+
+// FAQ ACCORDION
+
+// Listen for click on the document
+document.addEventListener('click', function (event) {
+
+  //Bail if our clicked element doesn't have the class
+  if (!event.target.classList.contains('accordion-toggle')) return;
+
+  // Get the target content
+  var content = document.querySelector(event.target.hash);
+  if (!content) return;
+
+  // Prevent default link behavior
+  event.preventDefault();
+
+  // If the content is already expanded, collapse it and quit
+  if (content.classList.contains('active')) {
+    content.classList.remove('active');
+    return;
+  }
+
+  // Get all open accordion content, loop through it, and close it
+  var accordions = document.querySelectorAll('.accordion-content.active');
+  for (var i = 0; i < accordions.length; i++) {
+    accordions[i].classList.remove('active');
+  }
+
+  // Toggle our content
+  content.classList.toggle('active');
+})
